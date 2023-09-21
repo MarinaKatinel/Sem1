7364039f28e42d59b52a891402b93f82905532cf
Marina <marika2392@yandex.ru>
2023-09-21 22:44:56 +0300

    Добавили Work1

---
+++ Desktop/ДЗ/Works/Work1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"max: {num1}; min: {num2}");
+}
+else
+{
+    Console.WriteLine($"max: {num2}; min:{num1}");
+}
