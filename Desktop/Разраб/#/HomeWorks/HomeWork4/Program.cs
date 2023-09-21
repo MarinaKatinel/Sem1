c9a3c5cd32d506347797eb91b0f45f93ac3f09d5
Marina <marika2392@yandex.ru>
2023-09-21 22:34:22 +0300

    Добавлены новые папки

---
+++ Desktop/Разраб/#/HomeWorks/HomeWork4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Чётные числа от 1 до {N}:");
+for (int i = 2; i <= N; i += 2)
+{
+    Console.Write(i + " ,");
+}
+
+Console.WriteLine();
