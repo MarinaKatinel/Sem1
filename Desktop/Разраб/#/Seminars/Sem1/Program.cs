f95b355b598781d984458ec7a9408007e6cb751f
Marina <marika2392@yandex.ru>
2023-09-21 14:43:39 +0300

    Update

---
+++ Desktop/Разраб/#/Seminars/Sem1/Program.cs
@@ -0,0 +1,16 @@
+﻿/
+{
+    private static void Main(string[] args)
+    {//System.Console.WriteLine("Input number: ");
+     //int num = Convert.ToInt32(Console.ReadLine());
+     //System.Console.WriteLine("Your number: "+num);
+
+//Программа вывода квадрата числа:
+//System.Console.WriteLine("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"Quad of {num}: {num*num}");
+
+//Программа, принимает 3хзначное число и выводит последнее число из этого числа
+
+/}
+}
