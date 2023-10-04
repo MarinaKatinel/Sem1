3eaf2bbbb78fe7e6d493282330c720cd753de7fb
Marina <marika2392@yandex.ru>
2023-10-04 12:54:26 +0300

    added Sem2

---
+++ Desktop/ДЗ/Works/Sem2/Program.cs
@@ -0,0 +1,113 @@
+﻿// Напишите программу , которая будет принимать на вход два числа и выводить, яляется ли второе число 
+// кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления
+
+//34,5 - не кратно, остаток 4
+// 16, 4 -кратно
+
+//void CheckMultiplicity(int num1, int num2)  //метод  void не возвращает число
+//{
+//    if(num1 % num2 == 0)
+//    {
+//        Console.WriteLine($"{num1}, {num2} -> кратно");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {num1 % num2}");
+//    }
+//}
+
+//Console.Write("Input first number:" );
+//int num1 =Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input second number:" );
+//int num2 =Convert.ToInt32(Console.ReadLine());
+
+//CheckMultiplicity(num1,num2);
+
+//Вторая задача:напишите программу, которая выврдит случайное число из отрезка {10, 99} и показывает 
+//наибольшую цифру числа
+
+//int BiggestDigit(int num)
+//{
+//    int ed = num % 10;
+//    int dec = num / 10;
+//    if(ed>dec)
+//    {
+//        return ed; // возвращает значение
+//    }
+//    else
+//    {
+//        return dec;
+//    }
+//}
+//
+//int num  = new Random().Next(10, 100); // метод Next передаем диапазон значений
+//Console.WriteLine(num);
+//int result = BiggestDigit(num);   *
+//Console.WriteLine(result);        *       
+//Console.WriteLine(BiggestDigit(num));
+
+//Третья задача:напишите программу, которая выводит случайное трехзначное число и удаляет
+//вторую цифру этого числа
+
+//void exampleMethod(int num)
+//{
+//    int hund = num /100;
+//    int ed = num %10;
+//    Console.WriteLine($"{num} -> {hund *10 +ed}");
+//}
+//int num  =  new Random().Next(100, 1000);
+//exampleMethod(num);
+
+//Четвертая задача: напишите программу, которая принимает на вход число и проверяет кратно ли
+//оно одновременно 7 и 23:
+
+//bool CheckMultiplicity(int num)
+//{
+//    if(num%7 ==0 && num% 23 == 0)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+//Console.WriteLine("Input number: ");
+//int num =Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"{num} -> {CheckMultiplicity(num)}");
+
+
+//ДЗ Задача : программа принимает на вход трехзначное число, и выводит вторую цифру этого числа
+
+//void CheckMultiplicity(int num1)
+//{
+//    int hund = num1 /10; //56
+//    int ed = hund %10; //6
+//    Console.WriteLine($"{num1} -> {ed}");
+//}
+//Console.Write("Input number:" );
+//int num1 =Convert.ToInt32(Console.ReadLine());
+//CheckMultiplicity(num1);
+
+//ДЗ 2 Задача: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+//3ДЗ Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//void searchWeekend(int num1)  //метод  void не возвращает число
+//{
+//    if(num1 >= 6)
+//    {
+//       Console.WriteLine($"{num1}-> this is off day ");
+//    }
+//   else
+//    {
+//        Console.WriteLine($"{num1}-> this is work day");
+//    }
+//}
+//
+//Console.Write("Input day of week:" );
+//int num1 =Convert.ToInt32(Console.ReadLine());
+//
+//searchWeekend(num1);
--- Desktop/ДЗ/Works/Work4/Program.cs
+++ Desktop/ДЗ/Works/Work4/Program.cs
@@ -1,9 +1,58 @@
-﻿Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Чётные числа от 1 до {N}:");
-for (int i = 2; i <= N; i += 2)
+﻿//Console.WriteLine("Введите число: ");
+//int N = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"Чётные числа от 1 до {N}:");
+//for (int i = 2; i <= N; i += 2)
+//{
+//    Console.Write(i + " ,");
+//}
+//
+//Console.WriteLine();
+
+//Нахождение максимум из 9 чисел:
+
+int Max(int arg1, int arg2, int arg3)
 {
-    Console.Write(i + " ,");
+    int result =arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
 }
 
-Console.WriteLine();
+//int a1 = 155641;
+//int b1 = 21;
+//int c1 = 39;
+//int a2 = 12;
+//int b2 = 2356;
+//int c2 = 33;
+//int a3 = 13;
+//int b3 = 23;
+//int c3 = 313;  
+
+//Массив:
+
+int[] array = {1,2,3,44,5,68,7,8,9};
+
+int max = Max(
+    Max (array[0], array[1],array[2]),
+    Max (array[3], array[4],array[5]),
+    Max (array[6], array[7],array[8])
+);
+Console.WriteLine(max);
+
+//int max1 = Max(a1, b1, c1);   Вторая правка
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+//if(b1 > max) max = b1;
+//if(c1 > max) max = c1;
+//if(a2 > max) max = a2;
+//if(b2 > max) max = b2;
+//if(c2 > max) max = c2;
+//if(a3 > max) max = a3;
+//if(b3 > max) max = b3;
+//if(c3 > max) max = c3;
+
+//int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+//Console.WriteLine(max);
